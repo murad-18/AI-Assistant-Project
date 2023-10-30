35b6ecdeae97493ae445e43ca9ab7a9662eb2d36
murad-18 <tikkamasala286@gmail.com>
2023-10-30 19:02:30 +0500

    created speak.py module

---
+++ Body/speak.py
@@ -0,0 +1,18 @@
+#speak functions - two types
+import pyttsx3
+#windows based
+def speak(text):
+    engine = pyttsx3.init("sapi5") # api to use windows voice
+    voices = engine.getProperty("voices")
+    engine.setProperty("voices",voices[0].id)
+    engine.setProperty("rate", 170)
+    print("")
+    print(f"You : {text}.")
+    engine.say(text)
+    engine.runAndWait()
+
+speak("I ask you till what end, dread it, run from it, destiny arives all the same")
+
+
+
+#chrome based 
