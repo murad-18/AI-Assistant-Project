25122469629a076fe38cb6da525d4db908f9b9a3
mujtabaraza194 <mujtabaraza194@gmail.com>
2023-10-31 18:18:41 +0500

    did nothing just tried

--- Body/listen.py
+++ Body/listen.py
@@ -1,15 +1,16 @@
-# Urdu or English - Command
-# Jarvis will reply in English
 import speech_recognition as sr
-from googletrans import Translator
+from translate import Translator
+
 # Listen function
+
+
 def listen():
     mcrphn = sr.Recognizer()
 
     with sr.Microphone() as source:
         print("Listening. . .")
         mcrphn.pause_threshold = 1
-        audio = mcrphn.listen(source, 0, 9) #Listening.....
+        audio = mcrphn.listen(source, 0, 9)  # Listening.....
         # mcrphn.adjust_for_ambient_noise(source, duration=3)
         # audio = mcrphn.listen(source)
 
@@ -23,23 +24,25 @@ def listen():
     except sr.RequestError as e:
         print(f"Could not request results; {e}")
         return ""
-    
+
     query = str(query).lower()
     return query
+
 
 # Translating function
 def translateUrduToEng(text):
     line = str(text)
-    translate = Translator()
-    result = translate.translate(line)
-    data = result.text
-    print(f"You : {data}." )
-    return data
+    translator = Translator(to_lang="en")
+    translation = translator.translate(line)
+    print(f"You: {translation}.")
+    return translation
+
 
 # connecting function
 def micConnection():
-    input = listen()
-    output = translateUrduToEng(input)
-    return output
+    input_text = listen()
+    output_text = translateUrduToEng(input_text)
+    return output_text
+
 
 micConnection()
